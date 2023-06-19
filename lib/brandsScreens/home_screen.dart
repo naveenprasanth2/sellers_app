69944f2795c0a0feda0fc7314e8d7bd362a5f8d2
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-06-19 12:04:13 +0530

    tillSection11

--- lib/brandsScreens/home_screen.dart
+++ lib/brandsScreens/home_screen.dart
@@ -55,6 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         body: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
           slivers: [
             const SliverPersistentHeader(
               pinned: true,
